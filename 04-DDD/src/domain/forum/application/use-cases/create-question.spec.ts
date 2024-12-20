8389fca340c5142b9b57b4dfedd8aa2c13d35379
guiogigo <guilhermevbatista20@gmail.com>
2024-12-20 01:48:23 -0300

    Inicio Clean Architecture

---
+++ 04-DDD/src/domain/forum/application/use-cases/create-question.spec.ts
@@ -0,0 +1,29 @@
+import { QuestionsRepository } from '../repositories/questions-repository'
+import { Question } from '../../enterprise/entities/question'
+import { CreateQuestionUseCase } from './create-question'
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let sut: CreateQuestionUseCase
+// sut : System Under Test
+
+describe('Create Question', () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    sut = new CreateQuestionUseCase(inMemoryQuestionsRepository)
+  })
+
+  it('should be able to create a question', async () => {  
+    const { question } = await sut.execute({
+      authorId: '1',
+      title: 'Nova pergunta',
+      content: 'Conteúdo da pergunta',
+    })
+  
+    expect(question.id).toBeTruthy()
+    expect(inMemoryQuestionsRepository.items[0].id).toEqual(question.id)
+
+  })
+})
+
+
